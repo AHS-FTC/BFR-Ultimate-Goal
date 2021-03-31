cb8a989d87f30b5a5012c92c12d89d22e90c14ec
appleby <alexappleby70@gmail.com>
2021-03-31 11:54:05 -0600

    finalized vision

--- TeamCode/src/main/java/com/bfr/opMode/StackDetectingOpMode.java
+++ TeamCode/src/main/java/com/bfr/opMode/StackDetectingOpMode.java
@@ -3,6 +3,7 @@ package com.bfr.opMode;
 import com.bfr.control.vision.StackDetector;
 import com.bfr.control.vision.VisionException;
 import com.bfr.control.vision.VisionSystem2;
+import com.bfr.util.AllianceColor;
 import com.bfr.util.FTCUtilities;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -15,12 +16,27 @@ public class StackDetectingOpMode extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         FTCUtilities.setOpMode(this);
 
+        if(gamepad1.b){
+            FTCUtilities.setAllianceColor(AllianceColor.RED);
+        } else if (gamepad1.x){
+            FTCUtilities.setAllianceColor(AllianceColor.BLUE);
+        }
+
         StackDetector sd = new StackDetector();
+
+        telemetry.addLine(FTCUtilities.getAllianceColor().toString());
+        telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()){
-            System.out.println(sd.getFieldConfiguration());
+            StackDetector.FieldConfiguration fc = sd.getFieldConfiguration();
+
+            System.out.println(fc);
+
+            telemetry.addLine(FTCUtilities.getAllianceColor().toString());
+            telemetry.addLine(fc.toString());
+            telemetry.update();
         }
 
         sd.stop();
